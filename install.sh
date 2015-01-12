1fd427623c82bbcc45f11bb907a11a06d45e894c
Jesús López <jeslopalo@sandbox.es>
2015-01-12 15:35:01 +0100

    Installing oh-my-zsh plugins

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ if [ ! -n "$SANDBIN_HOME" ]; then
 	SANDBIN_HOME=~/.sandbin
 fi
 
+if [ ! -n "$OM_ZSH_HOME" ]; then
+	OM_ZSH_HOME=~/.oh-my-zsh
+fi
+
 if [ -d "$SANDBIN_HOME" ]; then
 	echo "\033[0;33mYou already have sandbin installed.\033[0m You'll need to remove $SANDBIN_HOME if you want to install"
     	exit
@@ -14,3 +18,11 @@ hash git >/dev/null 2>&1 && env git clone https://github.com/jeslopalo/sandbin.g
   echo "git not installed"
   exit
 }
+
+if [ -d "$OM_ZSH_HOME" ]; then
+	echo "\033[0;33mInstalling zsh plugins...\033[0m"
+	mkdir -p "$OM_ZSH_HOME"/custom
+	# mkdir -p "$OM_ZSH_HOME"/custom/plugins
+	ln -s "$SANDBIN_HOME"/scripts/oh-my-zsh/plugins plugins
+
+fi
