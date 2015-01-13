e31092940b22a68271d6f7c9188141d8b82ef76c
Jesús López <jeslopalo@sandbox.es>
2015-01-13 15:36:45 +0100

    New iteration

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-#!/bin/zsh
 
 set -e
 
@@ -11,20 +10,56 @@ if [ ! -n "$OM_ZSH_HOME" ]; then
 fi
 
 if [ -d "$SANDBIN_HOME" ]; then
-	echo "\033[0;33mYou already have sandbin installed.\033[0m You'll need to remove $SANDBIN_HOME if you want to install"
+	echo "You already have sandbin installed. You'll need to remove $SANDBIN_HOME if you want to install"
     	exit
 fi
 
-echo "\033[0;34mCloning sandbin...\033[0m"
+echo "Cloning sandbin..."
 hash git >/dev/null 2>&1 && env git clone https://github.com/jeslopalo/sandbin.git $SANDBIN_HOME || {
   echo "git not installed"
   exit
 }
 
-if [ -d "$OM_ZSH_HOME" ]; then
-	echo "\033[0;33mInstalling zsh plugins...\033[0m"
-	mkdir -p "$OM_ZSH_HOME"/custom
-	# mkdir -p "$OM_ZSH_HOME"/custom/plugins
-	ln -s "$SANDBIN_HOME"/scripts/oh-my-zsh/plugins plugins
 
+if [ -d "$OM_ZSH_HOME" ]; then
+	echo -e "\nInstalling zsh plugins in $OM_ZSH_HOME/custom/plugins..."
+	
+	mkdir -p "$OM_ZSH_HOME"/custom
+	mkdir -p "$OM_ZSH_HOME"/custom/plugins
+
+	for plugin in $(dir "$SANDBIN_HOME/scripts/oh-my-zsh/plugins"); do
+		echo "- $SANDBIN_HOME/scripts/oh-my-zsh/plugins/$plugin to $OM_ZSH_HOME/custom/plugins/$plugin"
+		ln -s "$SANDBIN_HOME/scripts/oh-my-zsh/plugins/$plugin" "$OM_ZSH_HOME/custom/plugins/$plugin"
+	done
 fi
+
+
+function update_path() {
+	local config=$1;
+
+	if [ -f $config ]; then
+
+		if [[ ! ":$PATH:" == *":$SANDBIN_HOME/scripts:"* ]]; then
+			echo -e "\nUpdating $config PATH declaration..."
+
+			if grep -q "export PATH=$SANDBIN_HOME/scripts:\$PATH" $config; then
+				echo "sandbin is already declared in $config!"		
+			else
+				echo -e "\nexport PATH=$SANDBIN_HOME/scripts:\$PATH" >> $config
+				echo "sandbin directory has been declared in path!"
+			fi
+		fi
+#echo ps -p $$ 
+#
+#		if [[ "$config" == *`ps -p $$`* ]]; then
+#			echo "Sourcing $config file again"
+#			. $config
+#		fi
+	fi
+}
+
+zsh_config=~/.zshrc
+bash_config=~/.bashrc
+
+update_path "$bash_config"
+update_path "$zsh_config"
