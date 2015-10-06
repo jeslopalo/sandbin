fad60494d2d0877199618acd2c989a1221a1269c
Jesús López <jeslopalo@sandbox.es>
2015-10-06 17:18:03 +0200

    Add new alias: 'git authors'

--- scripts/git-init.sh
+++ scripts/git-init.sh
@@ -28,6 +28,8 @@ git config alias.changelog "log --pretty=oneline --abbrev-commit --decorate --co
 # TODO: See if it's possible to stop in previous release
 #git log v2.1.0...v2.1.1 --pretty=format:'<li> <a href="http://github.com/jeslopalo/<project>/commit/%H">view commit &bull;</a> %s</li> ' --reverse | grep "#changelog"
 
+echo -e "New command: git authors"
+git config alias.authors '!git log --all --format="%aN <%aE>" | sort -u'
 
 echo -e "Configuring aliases for unstage changes..."
 git config alias.unstage "reset HEAD"
