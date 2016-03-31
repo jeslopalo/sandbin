bbf749642a01009b81852bcd1db5d85678da6392
Jesús López Alonso <jeslopalo@sandbox.es>
2016-03-31 21:35:29 +0200

    Reveme unnecesary colors in install.sh

--- install.sh
+++ install.sh
@@ -100,18 +100,14 @@ if [ -n "$tput" ]; then
     ncolors=$(tput colors)
 fi
 if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-  RED="$(tput setaf 1)"
   GREEN="$(tput setaf 2)"
   YELLOW="$(tput setaf 3)"
   BLUE="$(tput setaf 4)"
-  BOLD="$(tput bold)"
   NORMAL="$(tput sgr0)"
 else
-  RED=""
   GREEN=""
   YELLOW=""
   BLUE=""
-  BOLD=""
   NORMAL=""
 fi
 
