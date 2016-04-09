d7e89a4fac798987d0ddde2c7a9254a80ec7b9d4
Jesús López Alonso <jeslopalo@sandbox.es>
2016-04-10 00:43:46 +0200

    Add new command 'gitbox setup user' to setup user.name in a git repository

--- scripts/lib/git-functions.lib.zsh
+++ scripts/lib/git-functions.lib.zsh
@@ -1,9 +1,32 @@
 
 function new_alias() {
-  local alias_name=$1
-  local alias_command=$2
+    local scope="$1"
+    local alias_name="$2"
+    local alias_command="$3"
 
-  printf "%s\n" "New command: ${YELLOW}git ${BOLD}${alias_name}${NORMAL}"
-  git config $scope alias.${alias_name} "${alias_command}"
+    if [ $# = 3 ]; then
+        printf "%s\n" "New command: ${YELLOW}git ${BOLD}${alias_name}${NORMAL}"
+        git config $scope alias.${alias_name} "${alias_command}"
+        exit 1
+    else
+        printf "%s\n" "New alias: scope, name or command not found"
+        exit 0
+    fi
 }
 
+function set_git_username() {
+    local scope="$1"
+    local username="$2"
+
+    if [ $# = 2 ]; then
+        git config $scope user.name "$username"
+    else
+        printf "${RED}%s${NORMAL}\n" "set_git_username: scope or username not found"
+    fi
+}
+
+function get_git_username() {
+    local scope="$1"
+
+    git config $scope --get user.name
+}
