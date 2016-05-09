9b736a3c5858d5ea878553dedb9167c5a60dcba3
Jesús López Alonso <jeslopalo@sandbox.es>
2016-05-09 23:09:54 +0200

    Extract installing functions into sandbin.lib.zsh

--- install.sh
+++ install.sh
@@ -79,75 +79,11 @@ else
     cp -R "$install_from_dir" $SANDBIN_HOME
 fi
 
-# replace function is in moustache.lib
-function replace() {
-    local pattern=$1;
-    local value=$2;
-    local file=$3;
-
-    if grep -q "{{$pattern}}" $file; then
-        printf "Replacing '%s' with '%s' in '%s' file\n" "$pattern" "$value" "$file"
-        local curated_value=${value//\//\\\/}
-        perl -pi -e "s/{{$pattern}}/\"$curated_value\"/g" "$file"
-    fi
-}
-
-function generate_sandbin_config_file() {
-    local sandbin_home=$1;
-    local sandbin_template="$sandbin_home/dotfiles/sandbin/sandbin.conf.template"
-    local sandbin_config="$sandbin_home/sandbin.conf"
-
-    if [ ! -f "$sandbin_config" ]; then
-        printf "Copying sandbin.conf file to '%s'...\n" "$sandbin_home"
-        cp "$sandbin_template" "$sandbin_config"
-
-        replace "sandbinhome" "$sandbin_home" "$sandbin_config"
-    fi
-}
-
-function remove_between_marks_in_file() {
-    local start_mark="$1";
-    local end_mark="$2";
-    local file="$3";
-
-    # Remove from content
-    sed "/$start_mark/,/$end_mark/d" "$file" > "$file.tmp"
-
-    # Backup old file before rewrite
-    /bin/cp "$file" "$file.oldbackup"
-    /bin/mv "$file.tmp" "$file"
-}
-
-function configure_sandbin_bootstrap() {
-    local config_file=$1;
-    local sandbin_home=$2;
-    local sandbin_home_sha=`echo $sandbin_home | /usr/bin/shasum | /usr/bin/cut -c 1-10`;
-    local START_MARK="#sandbin-bootstrap";
-    local END_MARK="#end-sandbin-bootstrap";
-    local sandbin_config="$sandbin_home/sandbinrc";
-
-    if [ -f $config_file ]; then
-
-        if grep -q "$START_MARK $sandbin_home_sha" "$config_file"; then
-            echo "The sandbin bootstrap is already configured in '$config_file'"
-
-        else
-            if grep -q "$START_MARK" "$config_file"; then
-                echo "Removing old sandbin configuration from '$config_file' file"
-                remove_between_marks_in_file "$START_MARK" "$END_MARK\\n" "$config_file"
-            fi
-
-            printf "%s\n%s\n%s\n" "$START_MARK $sandbin_home_sha" "source $sandbin_config" "$END_MARK" >> "$config_file"
-            echo "The sandbin bootstrap '$sandbin_config' has been configured in '$config_file'"
-        fi
-    fi
-}
+source "${SANDBIN_HOME}/scripts/sandbin/lib/sandbin.lib.zsh"
 
 generate_sandbin_config_file "$SANDBIN_HOME"
 configure_sandbin_bootstrap ~/.bashrc "$SANDBIN_HOME"
 configure_sandbin_bootstrap ~/.zshrc "$SANDBIN_HOME"
-
-source "${SANDBIN_HOME}/scripts/sandbin/lib/sandbin.lib.zsh"
 
 print_sandbin_banner "$(sandbin_version)" "${GREEN}${BOLD}" "sandbin"
 printf "%s\n" "${BLUE}Hooray! Sandbin has been installed.${NORMAL}"
--- scripts/sandbin/lib/sandbin.lib.zsh
+++ scripts/sandbin/lib/sandbin.lib.zsh
@@ -1,3 +1,58 @@
+source "$SANDBIN_HOME/scripts/lib/moustache.lib.zsh"
+
+function generate_sandbin_config_file() {
+    local sandbin_home=$1;
+    local sandbin_template="$sandbin_home/dotfiles/sandbin/sandbin.conf.template"
+    local sandbin_config="$sandbin_home/sandbin.conf"
+
+    if [ ! -f "$sandbin_config" ]; then
+        printf "Copying sandbin.conf file to '%s'...\n" "$sandbin_home"
+        cp "$sandbin_template" "$sandbin_config"
+
+        replace "sandbinhome" "$sandbin_home" "$sandbin_config"
+    fi
+}
+
+function remove_between_marks_in_file() {
+    local start_mark="$1";
+    local end_mark="$2";
+    local file="$3";
+
+    # Remove from content
+    sed "/$start_mark/,/$end_mark/d" "$file" > "$file.tmp"
+
+    # Backup old file before rewrite
+    /bin/cp "$file" "$file.oldbackup"
+    /bin/mv "$file.tmp" "$file"
+}
+
+function configure_sandbin_bootstrap() {
+    local config_file=$1;
+    local sandbin_home=$2;
+    local sandbin_home_sha=`echo $sandbin_home | /usr/bin/shasum | /usr/bin/cut -c 1-10`;
+    local START_MARK="#sandbin-bootstrap";
+    local END_MARK="#end-sandbin-bootstrap";
+    local sandbin_config="$sandbin_home/sandbinrc";
+
+    if [ -f $config_file ]; then
+
+        if grep -q "$START_MARK $sandbin_home_sha" "$config_file"; then
+            echo "The sandbin bootstrap is already configured in '$config_file'"
+
+        else
+            if grep -q "$START_MARK" "$config_file"; then
+                echo "Removing old sandbin configuration from '$config_file' file"
+                remove_between_marks_in_file "$START_MARK" "$END_MARK\\n" "$config_file"
+            fi
+
+            printf "%s\n%s\n%s\n" "$START_MARK $sandbin_home_sha" "source $sandbin_config" "$END_MARK" >> "$config_file"
+            echo "The sandbin bootstrap '$sandbin_config' has been configured in '$config_file'"
+        fi
+    fi
+}
+
+### BANNER functions
+
 function random_file_name() {
     local directory="$1"
 
