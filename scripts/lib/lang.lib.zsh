a3392b97f98e49f57ce13c038a871cb2d3c28324
Jesús López Alonso <jeslopalo@sandbox.es>
2016-05-25 22:51:04 +0200

    New contains() function

--- scripts/lib/lang.lib.zsh
+++ scripts/lib/lang.lib.zsh
@@ -2,6 +2,20 @@
 function import {
   local -r file="$1"
   shift
-  # shellcheck source=$file
+
   source "$SANDBIN_HOME/$file" "$@"
+}
+
+# contains(string, substring)
+#
+# Returns 0 if the specified string contains the specified substring,
+# otherwise returns 1.
+#
+# From: http://stackoverflow.com/a/8811800
+#
+function contains() {
+    local string="$1"
+    local substring="$2"
+
+    return $(test "${string#*$substring}" != "$string")
 }
