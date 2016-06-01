3650d46b7b9756e3830d8d9ee42017d3509eb9ea
Jesús López Alonso <jeslopalo@sandbox.es>
2016-06-01 17:36:12 +0200

    Refactor sandbin.conf.template location

--- scripts/sandbin/lib/sandbin.lib.zsh
+++ scripts/sandbin/lib/sandbin.lib.zsh
@@ -1,12 +1,14 @@
 import "scripts/lib/moustache.lib.zsh"
 
+CONFIG_TEMPLATE_LOCATION="scripts/sandbin/sandbin.conf.template"
+
 function generate_sandbin_config_file() {
     local sandbin_home=$1;
-    local sandbin_template="$sandbin_home/dotfiles/sandbin/sandbin.conf.template"
+    local sandbin_template="$sandbin_home/$CONFIG_TEMPLATE_LOCATION"
     local sandbin_config="$sandbin_home/sandbin.conf"
 
     if [ ! -f "$sandbin_config" ]; then
-        printf "Copying sandbin.conf file to '%s'...\n" "$sandbin_home"
+        printf "${BOLD}Copying sandbin.conf file${NORMAL} template ('%s') to '%s' directory...\n" "$sandbin_template" "$sandbin_home"
         cp "$sandbin_template" "$sandbin_config"
 
         replace "sandbinhome" "$sandbin_home" "$sandbin_config"
@@ -37,16 +39,16 @@ function configure_sandbin_bootstrap() {
     if [ -f $config_file ]; then
 
         if grep -q "$START_MARK $sandbin_home_sha" "$config_file"; then
-            echo "The sandbin bootstrap is already configured in '$config_file'"
+            echo "The ${BOLD}sandbin bootstrap is already configured${NORMAL} in '$config_file'"
 
         else
             if grep -q "$START_MARK" "$config_file"; then
-                echo "Removing old sandbin configuration from '$config_file' file"
+                echo "${BOLD}Removing old sandbin configuration${NORMAL} from '$config_file' file"
                 remove_between_marks_in_file "$START_MARK" "$END_MARK\\n" "$config_file"
             fi
 
             printf "%s\n%s\n%s\n" "$START_MARK $sandbin_home_sha" "source $sandbin_config" "$END_MARK" >> "$config_file"
-            echo "The sandbin bootstrap '$sandbin_config' has been configured in '$config_file'"
+            echo "${BOLD}The sandbin bootstrap '$sandbin_config' has been configured${NORMAL} in '$config_file'"
         fi
     fi
 }
